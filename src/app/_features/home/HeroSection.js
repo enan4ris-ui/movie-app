e9fbbd0087a7d8c48d84fa73e5a623f94da978bc
enan4ris-ui <enan4ris@gmail.com>
2025-11-04 12:37:27 +0800

    changes

--- src/app/_features/home/HeroSection.js
+++ src/app/_features/home/HeroSection.js
@@ -1,15 +1,28 @@
 "use client";
 
 import React from "react";
-import { useEffect, useState } from "react";
+import { WatchTrailerIcon } from "../_icons/WatchTrailerIcon";
+import { StarIcon } from "@/app/_features/_icons/StarIcon";
 import { Card, CardContent } from "@/components/ui/card";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 import {
   Carousel,
   CarouselContent,
   CarouselItem,
   CarouselNext,
   CarouselPrevious,
+  CarouselDots,
 } from "@/components/ui/carousel";
+import { useEffect, useState } from "react";
 import { LoadingMovieList } from "./LoadingMovieList";
 const BASE_URL = "https://api.themoviedb.org/3";
 const ACCESS_TOKEN =
@@ -17,63 +30,143 @@ const ACCESS_TOKEN =
 
 export function HeroSection() {
   const [loading, setLoading] = useState(false);
-  const [nowPlayingDataList, setNowPlayingDataList] = useState([]);
-  const getPopularDataList = async () => {
+  const [heroSectionData, setHeroSectionData] = useState([]);
+
+  const [selectedMovieId, setSelectedMovieId] = useState(null);
+  const [movieTrailer, setMovieTrailer] = useState({});
+  const [trailerLoading, setTrailerLoading] = useState(false);
+
+  const getHeroSectionData = async () => {
     setLoading(true);
-
-    const nowPlayingEndpoint = `${BASE_URL}/movie/now_playing?language=en-US&page=1`;
-
-    const response = await fetch(nowPlayingEndpoint, {
+    const heroSectionEndpoint = `${BASE_URL}/movie/now_playing?language=en-US&page=1`;
+    const response = await fetch(heroSectionEndpoint, {
       headers: {
         Authorization: `Bearer ${ACCESS_TOKEN}`,
         "Content-Type": "application/json",
       },
     });
+
     const data = await response.json();
 
-    setNowPlayingDataList(data.results);
-    setLoading(false);
+    setHeroSectionData(data.results);
+    setTimeout(() => {
+      setLoading(false);
+    }, "2000");
   };
 
   useEffect(() => {
-    getPopularDataList();
+    getHeroSectionData();
   }, []);
 
-  if (loading) {
-    console.log(<LoadingMovieList />);
-  }
+  const getMovieVideos = async () => {
+    setTrailerLoading(true);
+    const heroSectionEndpoint = `${BASE_URL}/movie/${selectedMovieId}/videos?language=en-US`;
+    const response = await fetch(heroSectionEndpoint, {
+      headers: {
+        Authorization: `Bearer ${ACCESS_TOKEN}`,
+        "Content-type": "application/json",
+      },
+    });
+
+    const data = await response.json();
+
+    setMovieTrailer(
+      data.results.find((trailer) => {
+        if (trailer.name.includes("Official Trailer")) {
+          return trailer;
+        }
+      })
+    );
+    setTimeout(() => {
+      setTrailerLoading(false);
+    }, "2000");
+  };
+
+  useEffect(() => {
+    console.log("id is getting change, call api again");
+    if (selectedMovieId !== null) {
+      getMovieVideos();
+    }
+  }, [selectedMovieId]);
+
+  const handleWatchTrailerButton = (id) => {
+    setSelectedMovieId(id);
+  };
+
+  if (loading)
+    return (
+      <div>
+        <LoadingMovieList />
+      </div>
+    );
+
+  console.log(movieTrailer, "movieTrailermovieTrailermovieTrailer");
 
   return (
-    <Carousel className="w-[1440px] max-w">
-      <CarouselContent>
-        {nowPlayingDataList.slice(0, 5).map((movie, index) => (
-          <CarouselItem key={index}>
-            <div className="p-1">
-              <Card>
-                {/* <CardContent className="flex aspect-square items-center justify-center p-6">
-                  <span className="text-4xl font-semibold">{index + 1}</span>
-                  <span className="text-4xl font-semibold">{movie.title}</span>
-
-                  <img src="https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1"></img>
-                </CardContent>{" "} */}
-                <CardContent
-                  className="p-0 w-[1440px] min-h-[600px] bg-cover bg-center"
-                  style={{
-                    backgroundImage: `url(https://image.tmdb.org/t/p/original${movie.backdrop_path})`,
-                  }}
-                >
-                  {" "}
-                  <div>
-                    <h2 className="text-4xl font-semibold">{movie.title}</h2>
-                  </div>
-                </CardContent>
-              </Card>
-            </div>
-          </CarouselItem>
-        ))}
-      </CarouselContent>
-      <CarouselPrevious />
-      <CarouselNext />
-    </Carousel>
+    <div className="flex justify-center items-center w-full">
+      <Carousel className="w-full">
+        <CarouselContent>
+          {heroSectionData.slice(0, 5).map((movie, index) => (
+            <CarouselItem key={index}>
+              <div>
+                <Card>
+                  <CardContent
+                    className="relative bg-no-repeat p-0 w-full h-[600px] bg-cover bg-center"
+                    style={{
+                      backgroundImage: `url(https://image.tmdb.org/t/p/original${movie.backdrop_path})`,
+                    }}
+                  >
+                    <div className="flex flex-col items-start gap-4 absolute left-35 bottom-40 w-[404px]">
+                      <div>
+                        <span className="text-white font-inter text-base font-normal leading-6">
+                          Now Playing:
+                        </span>
+                        <p className="text-white font-inter text-4xl font-bold leading-[40px] tracking[-0.9px]">
+                          {movie.title}
+                        </p>
+                        <div className="flex gap-1 items-center">
+                          <StarIcon />
+                          <p className="text-[#FAFAFA] font-inter text-lg font-semibold leading-7">
+                            {movie.vote_average}
+                          </p>
+                          <div className="text-[#FAFAFA] font-inter text-xs font-normal leading-4 w-[302px]">
+                            {movie.overview}
+                          </div>
+                          <div className="flex h-[40px] py-2 px-4 justify-center items-center gap-2 rounded-md bg-[#F4F4F5]">
+                            <WatchTrailerIcon />
+                            <button
+                              onClick={() => handleWatchTrailerButton(movie.id)}
+                              style={{ cursor: "pointer" }}
+                              className="text-[var(--text-text-secondary-foreground)] font-inter text-sm font-medium leading-5"
+                            >
+                              Watch Trailer
+                            </button>
+                          </div>
+                        </div>
+                      </div>
+                    </div>{" "}
+                  </CardContent>
+                </Card>
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious className="bg-[#FAFAFA]" />
+        <CarouselNext className="bg-[#FAFAFA]" />
+      </Carousel>
+      {selectedMovieId && trailerLoading && <div>trailer loading</div>}
+      {selectedMovieId && !trailerLoading && (
+        <div>
+          <iframe
+            width={560}
+            height={315}
+            src={`https://www.youtube.com/embed/${movieTrailer.key}`}
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+        </div>
+      )}
+    </div>
   );
 }
--- src/app/_features/home/MovieList.js
+++ src/app/_features/home/MovieList.js
@@ -6,12 +6,11 @@ import { useEffect, useState } from "react";
 import { MovieCard } from "../../_components/MovieCard";
 import { ArrowRight } from "../_icons/ArrowRight";
 import { useRouter } from "next/navigation";
-
 const BASE_URL = "https://api.themoviedb.org/3";
 const ACCESS_TOKEN =
   "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxMjI5ZmNiMGRmZTNkMzc2MWFmOWM0YjFjYmEyZTg1NiIsIm5iZiI6MTc1OTcxMTIyNy43OTAwMDAyLCJzdWIiOiI2OGUzMGZmYjFlN2Y3MjAxYjI5Y2FiYmIiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.M0DQ3rCdsWnMw8U-8g5yGXx-Ga00Jp3p11eRyiSxCuY";
 
-export const MovieList = ({ type }) => {
+export const MovieList = ({ type, seeMore }) => {
   const router = useRouter();
   const [popularMoviesData, setPopularMoviesData] = useState([]);
   const [loading, setLoading] = useState(false);
@@ -37,41 +36,41 @@ export const MovieList = ({ type }) => {
     getPopularDataList();
   }, []);
 
-  if (loading) {
-    return <LoadingMovieList />;
-  }
+  // if (loading) {
+  //   return <LoadingMovieList />;
+  // }
 
   const handleSeeMoreButton = () => {
     router.push(`/movies/${type}`);
   };
 
   return (
-    <div className="w-[1277px] h-[978px] flex flex-col justify-between">
+    <div className="flex flex-col justify-between px-[80px] gap-x-8 gap-8">
       <div className="flex justify-between items-center">
-        <h3 className="font-semibold inter text-[24px]">
+        <p className="text-[var(--text-text-foreground)] font-inter text-2xl font-semibold leading-8 tracking-[-0.6px]">
           {type.toUpperCase()}
-        </h3>
-
-        <button
-          onClick={handleSeeMoreButton}
-          className="flex items-center justify-center text-[14px] text-[#09090B] gap-2 cursor-pointer"
-        >
-          See More
-          <ArrowRight className="w-4 h-4" />
-        </button>
-      </div>
-
-      <div className="w-[1277px] h-[910px] overflow-hidden">
-        <div className="grid grid-rows-2 grid-cols-5 gap-8 top-[191px] left-[52px]">
-          {popularMoviesData.map((movie, index) => (
-            <MovieCard
-              key={index}
-              title={movie.title}
-              imageURL={movie.poster_path}
-              rating={movie.vote_average}
-            />
-          ))}
+        </p>
+        <div className="flex h-[36px] py-2 px-4 justify-center items-center gap-2 rounded-md border border-[#E4E4E7 bg-white shadow-sm">
+          {seeMore && (
+            <button
+              onClick={handleSeeMoreButton}
+              className="flex items-center justify-center text-[14px] text-[#09090B] gap-2 cursor-pointer"
+            >
+              See More
+              <ArrowRight className="w-4 h-4" />
+            </button>
+          )}
         </div>
+      </div>
+      <div className="w-[1277px] h-[910px] flex items-start content-start self-stretch flex-wrap gap-x-8 gap-y-8">
+        {popularMoviesData.slice(0, 10).map((movie, index) => (
+          <MovieCard
+            key={index}
+            title={movie.title}
+            imageURL={movie.poster_path}
+            rating={movie.vote_average}
+          />
+        ))}
       </div>
     </div>
   );
