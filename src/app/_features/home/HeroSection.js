3a680b80ccdf73cd409ec0138330669745fa831d
enan4ris-ui <enan4ris@gmail.com>
2025-11-05 12:28:59 +0800

    cahnges

--- src/app/_features/home/HeroSection.js
+++ src/app/_features/home/HeroSection.js
@@ -103,7 +103,7 @@ export function HeroSection() {
   console.log(movieTrailer, "movieTrailermovieTrailermovieTrailer");
 
   return (
-    <div className="flex justify-center items-center w-full">
+    <div className="flex justify-center items-center flex-col w-full">
       <Carousel className="w-full">
         <CarouselContent>
           {heroSectionData.slice(0, 5).map((movie, index) => (
@@ -116,32 +116,60 @@ export function HeroSection() {
                       backgroundImage: `url(https://image.tmdb.org/t/p/original${movie.backdrop_path})`,
                     }}
                   >
-                    <div className="flex flex-col items-start gap-4 absolute left-35 bottom-40 w-[404px]">
-                      <div>
+                    <div className="flex flex-col text-left pl-[70px] items-start gap-4 absolute left-35 bottom-40 w-[404px]">
+                      <div className="">
                         <span className="text-white font-inter text-base font-normal leading-6">
                           Now Playing:
                         </span>
                         <p className="text-white font-inter text-4xl font-bold leading-[40px] tracking[-0.9px]">
                           {movie.title}
                         </p>
-                        <div className="flex gap-1 items-center">
-                          <StarIcon />
-                          <p className="text-[#FAFAFA] font-inter text-lg font-semibold leading-7">
-                            {movie.vote_average}
-                          </p>
-                          <div className="text-[#FAFAFA] font-inter text-xs font-normal leading-4 w-[302px]">
+                        <div className="flex flex-col gap-1 items-start">
+                          <div className="flex flex-row">
+                            <StarIcon />
+
+                            <p className="text-[#FAFAFA] flex flex-row font-inter text-lg font-semibold leading-7">
+                              {movie.vote_average} /10
+                            </p>
+                          </div>
+                          <div className="text-[#FAFAFA] pt-[14px] font-inter text-xs font-normal leading-4 w-[302px]">
                             {movie.overview}
                           </div>
-                          <div className="flex h-[40px] py-2 px-4 justify-center items-center gap-2 rounded-md bg-[#F4F4F5]">
-                            <WatchTrailerIcon />
-                            <button
-                              onClick={() => handleWatchTrailerButton(movie.id)}
-                              style={{ cursor: "pointer" }}
-                              className="text-[var(--text-text-secondary-foreground)] font-inter text-sm font-medium leading-5"
-                            >
-                              Watch Trailer
-                            </button>
-                          </div>
+                          <Dialog>
+                            <form>
+                              <DialogTrigger
+                                asChild
+                                className="flex h-[40px] py-2 px-4 justify-center items-center gap-2 rounded-md bg-[#F4F4F5]"
+                              >
+                                <button
+                                  onClick={() =>
+                                    handleWatchTrailerButton(movie.id)
+                                  }
+                                  style={{ cursor: "pointer" }}
+                                  //   className="text-[var(--text-text-secondary-foreground)] font-inter text-sm font-medium leading-5 animate-bounce" //
+                                >
+                                  <WatchTrailerIcon />
+                                  Watch Trailer
+                                </button>
+                              </DialogTrigger>
+                              <DialogContent className="sm:max-w-[425px] flex justify-center">
+                                <div>
+                                  <iframe
+                                    width={997}
+                                    height={562}
+                                    src={`https://www.youtube.com/embed/${movieTrailer.key}`}
+                                    frameborder="0"
+                                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                    allowFullScreen
+                                  ></iframe>
+                                </div>
+                                <DialogHeader className="display: hidden">
+                                  {" "}
+                                  {movie.title}
+                                </DialogHeader>
+                              </DialogContent>
+                            </form>
+                          </Dialog>
                         </div>
                       </div>
                     </div>{" "}
@@ -154,7 +182,7 @@ export function HeroSection() {
         <CarouselPrevious className="bg-[#FAFAFA]" />
         <CarouselNext className="bg-[#FAFAFA]" />
       </Carousel>
-      {selectedMovieId && trailerLoading && <div>trailer loading</div>}
+      {/* {selectedMovieId && trailerLoading && <div>trailer loading</div>}
       {selectedMovieId && !trailerLoading && (
         <div>
           <iframe
@@ -166,7 +194,7 @@ export function HeroSection() {
             allowFullScreen
           ></iframe>
         </div>
-      )}
+      )} */}
     </div>
   );
 }
