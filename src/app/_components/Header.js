8171453f112db67ce6bfb175ae67355b6265a2f8
enan4ris-ui <enan4ris@gmail.com>
2025-11-07 12:29:24 +0800

    add

--- src/app/_components/Header.js
+++ src/app/_components/Header.js
@@ -1,6 +1,21 @@
 import MoonIcon from "../_features/_icons/MoonIcon";
 import FilmIcon from "../_features/_icons/FilmIcon";
-import HeaderIcon from "../_features/_icons/HeaderIcon";
+import { Button } from "@/components/ui/button";
+import { ArrowDown } from "../_features/_icons/ArrowDown";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 export const Header = () => {
   return (
     <div className=" p-4 w-[1440px] h-[59px] flex flex-row justify-around">
@@ -13,10 +28,23 @@ export const Header = () => {
       </div>
 
       <div className="flex items-center gap-3">
-        <button className="rounded-md border border-[#E4E4E7] bg-white shadow-sm flex w-[97px] h-9 px-4 py-2 justify-center items-center gap-1.5">
-          <HeaderIcon />
-          Genre
-        </button>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <ArrowDown />
+            <Button
+              className="border border-[#E4E4E7] text-[14px] font-semibold"
+              variant="outline"
+            >
+              Genre
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent
+            className="w-[577px] h-[333px] bg-white border-gray-200"
+            align="start"
+          >
+            <p className="font-sans font-semibold text-[24px] p-5">Genres</p>
+          </DropdownMenuContent>
+        </DropdownMenu>
         <input
           className="flex w-[379px] h-9 px-3 items-center gap-2.5 rounded-lg border border-[#E4E4E7] bg-white"
           placeholder="Search"
--- src/app/_components/MovieCard.js
+++ src/app/_components/MovieCard.js
@@ -3,17 +3,17 @@ const { StarIcon } = require("../_features/_icons/StarIcon");
 export function MovieCard(props) {
   const { title, imageURL, rating } = props;
   return (
-    <div className="w-[229.73px] h-[439px] bg-[#F4F4F5] rounded flex flex-col justify-end cursor-pointer">
+    <div className="w-[229.73px] h-[439px] bg-[#F4F4F5] object-cover rounded-lg flex flex-col justify-end cursor-pointer">
       <div
         style={{
           backgroundImage: `url('https://image.tmdb.org/t/p/original${imageURL}')`,
         }}
-        className={`w-[229.73px] h-[340px] bg-center bg-cover`}
+        className={`w-[229.73px] h-[340px] bg-center rounded-t-lg bg-cover`}
       ></div>
       <div className="w-[229.73px] h-[95px] p-2">
         <div className="flex flex-col items-start">
           <div className="flex ">
-            <StarIcon className="w-4 h-[18px] object-contain" />
+            <StarIcon className="w-4 h-[18px] " />
             <p className="font-medium pl-1">{rating}</p>
             <p className="text-[#71717A] not-[]:">/10</p>
           </div>
